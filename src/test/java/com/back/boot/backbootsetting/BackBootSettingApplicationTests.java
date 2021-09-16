77d59e45d00779043199ab05ad8ac85ae7aa884f
reolinodev <reolino.dev@gmail.com>
2021-09-16 17:10:55 +0900

    스프링 초기 파일 커밋

---
+++ src/test/java/com/back/boot/backbootsetting/BackBootSettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.boot.backbootsetting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackBootSettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
