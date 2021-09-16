69822ea3230bed519c276c2e47753aed0f03cb53
reolinodev <reolino.dev@gmail.com>
2021-09-16 19:54:36 +0900

    폴더 구조 변경

--- src/main/java/com/back/BackEndSettingApplication.java
+++ src/main/java/com/back/BackEndSettingApplication.java
@@ -1,13 +1,13 @@
-package com.back.boot.backbootsetting;
+package com.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackBootSettingApplication {
+public class BackEndSettingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackBootSettingApplication.class, args);
+		SpringApplication.run(BackEndSettingApplication.class, args);
 	}
 
 }
--- src/test/java/com/back/BackEndSettingApplicationTests.java
+++ src/test/java/com/back/BackEndSettingApplicationTests.java
@@ -1,10 +1,10 @@
-package com.back.boot.backbootsetting;
+package com.back;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackBootSettingApplicationTests {
+class BackEndSettingApplicationTests {
 
 	@Test
 	void contextLoads() {
