0d6244734e247a3bf2e14fe3678e97c1e2468b52
reolinodev <reolino.dev@gmail.com>
2021-09-23 10:35:09 +0900

    aop 적용 (서비스 시간 체크)

---
+++ src/main/java/com/back/config/TimeTraceAop.java
@@ -0,0 +1,24 @@
+package com.back.config;
+
+import org.springframework.stereotype.Component;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Component
+@Aspect
+public class TimeTraceAop {
+	@Around("execution(* com.back.service..*(..))")
+	public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+		long start = System.currentTimeMillis();
+		System.out.println("START: " + joinPoint.toString());
+
+		try {
+			return joinPoint.proceed();
+		} finally {
+			long finish = System.currentTimeMillis();
+			long timeMs = finish - start;
+			System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
+		}
+	}
+}
