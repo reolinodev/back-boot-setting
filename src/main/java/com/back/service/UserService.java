c11377bae8acf5d665b8fbcf691d5d7a4f322614
reolinodev <reolino.dev@gmail.com>
2021-09-25 18:32:25 +0900

    서비스 단 정리

--- src/main/java/com/back/service/UserService.java
+++ src/main/java/com/back/service/UserService.java
@@ -14,7 +14,8 @@ public class UserService {
 	* 사용자를 전체조회 합니다.
 	*/
 	public ArrayList<User> findAll(HashMap<String, Object> params) {
-		ArrayList<User> result = new ArrayList<User>();
+        System.out.println("findAll이 실행되었습니다");
+        ArrayList<User> result = new ArrayList<User>();
         User user1 = new User();
         user1.setId("1");
         user1.setEmail("aaa@gmail.com");
@@ -36,6 +37,7 @@ public class UserService {
 	* 사용자를 상세 조회 합니다.
 	*/
     public User findById(String id) {
+        System.out.println("findById 실행되었습니다");
         User user = new User();
         user.setId("1");
         user.setEmail("aaa@gmail.com");
@@ -49,8 +51,8 @@ public class UserService {
 	* 사용자를 생성 합니다.
 	*/
     public int create(User user) {
-	    int result = 1;
         System.out.println("create가 실행되었습니다");
+	    int result = 1;
         return result;
 	}
     
@@ -58,8 +60,8 @@ public class UserService {
 	* 사용자 정보를 수정합니다.
 	*/
     public int update(User user) {
-	    int result = 1;
         System.out.println("update가 실행되었습니다");
+	    int result = 1;
         return result;
 	}
 
@@ -67,8 +69,8 @@ public class UserService {
 	* 사용자를 삭제 합니다.
 	*/
     public int delete(String id) {
-	    int result = 1;
         System.out.println("delete가 실행되었습니다");
+	    int result = 1;
         return result;
 	}
 }
