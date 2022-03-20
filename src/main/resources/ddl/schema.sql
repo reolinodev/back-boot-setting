e9b6dc91fe5c8893d70c857ecc6b5a639119723f
reolinodev <reolino.dev@gmail.com>
2022-03-20 21:56:36 +0900

    mariadb 설정 추가

--- src/main/resources/ddl/schema.sql
+++ src/main/resources/ddl/schema.sql
@@ -1,12 +1,12 @@
 CREATE TABLE TB_USER
 (
-    id Long NOT NULL,
+    id int NOT NULL,
     name varchar(30) not null,
     email varchar(50) not null,
     birth varchar(8),
     password varchar(255) not null,
     phone varchar(20),
-    indate timestamp,
-    ldate timestamp,
+    created_at timestamp,
+    updated_at timestamp,
     primary key(id)
 );
