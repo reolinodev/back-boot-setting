bce78faaf94741dac4b007ea9eb138e497802cd5
reolinodev <reolino.dev@gmail.com>
2022-03-25 15:22:37 +0900

    eslint 설정 변경
    (cherry picked from commit 1021e8480280315d9620391ad422f5b749106b5e)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
     extends: ['airbnb-base', 'plugin:node/recommended', 'prettier'],
+    parserOptions: {
+        parser: 'babel-eslint',
+        sourceType: 'module',
+        allowImportExportEverywhere: true,
+        ecmaVersion: 11,
+    },
 };
