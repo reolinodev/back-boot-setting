e3f2b8061f182ab5c6548ecd9e6d90135675e55d
reolinodev <reolino.dev@gmail.com>
2022-03-29 14:49:55 +0900

    eslint rule 추가
    (cherry picked from commit 391b3a14d69ed0c1fa13d1a20180a5ef67937d70)

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,4 +6,10 @@ module.exports = {
         allowImportExportEverywhere: true,
         ecmaVersion: 11,
     },
+    rules: {
+        'node/no-unsupported-features/es-syntax': [
+            'error',
+            { ignores: ['modules'] },
+        ],
+    },
 };
